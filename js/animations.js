90c1efb8688cdd165d6d9e1b9a785cd057747526
Jesrom <jesparzarom@outlook.com>
2023-05-11 17:00:37 -0300

    Mejorando Nav | Modificando animaciones | mejorando css de  .container | Cambiando color de fondo y Nav| Mejorando header página de tragos

--- js/animations.js
+++ js/animations.js
@@ -1,14 +1,21 @@
 VANTA.FOG({
-    el: ".body",
+    el: ".bkg-anim",
     mouseControls: true,
     touchControls: true,
     gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    highlightColor: 0xff0000,
+    minHeight: 0,
+    minWidth: 0,
+/*     highlightColor: 0xff0000,
     midtoneColor: 0x62057a,
     lowlightColor: 0xd00ff,
-    baseColor: 0xf06adf,
+    baseColor: 0xf06adf, */
+    
+    highlightColor: 0xf5950c,
+    midtoneColor: 0x5a0887,
+    lowlightColor: 0xff0000,
+    baseColor: 0xffe0c3,
+    
     blurFactor: 0.46,
-    zoom: 0.60
+    zoom: 0.60,
+    speed: 1,
 })
