1de7059c9663f2502f20599393bf1c084f13041a
Jesrom <jesparzarom@outlook.com>
2023-05-18 13:11:24 -0300

    form: agregando validación, animaciónes en validación y estilos | mejorando estilos en general | cambiando animación de indexcolores. | quitando links innecesarios (a revisar) | NavBar test y aside Bar responsive

--- js/form.js
+++ js/form.js
@@ -1,22 +1,67 @@
+let nameIn = document.querySelector(".alert-name");
+
 function validateForm() {
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var message = document.getElementById("message").value;
-
-    if (name == "" || email == "" || message == "") {
-        alert("Por favor, rellene todos los campos requeridos.");
-        return false;
+    // Obtener los valores de los campos
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let subject = document.getElementById("subject").value;
+    let message = document.getElementById("message").value;
+    let validAlert = "";
+  
+    // Validar los campos (puedes agregar tus propias validaciones aquí)
+  
+    // Comprobar si el campo de nombre está vacío
+    if (name === '') {
+      validAlert = 'Por favor, ingresa tu nombre';
+      notValid(validAlert);
+      return false; // Detener el envío del formulario
     }
+  
+    // Comprobar si el campo de correo electrónico está vacío o no es válido
+    if (email === '') {
+      validAlert = 'Por favor, ingresa tu correo electrónico';
+      notValid(validAlert);
+      return false; // Detener el envío del formulario
 
-    if (!validateEmail(email)) {
-        alert("Por favor, ingrese una dirección de correo electrónico válida.");
-        return false;
+    } else if (!validateEmail(email)) {
+      validAlert = 'Por favor, ingresa tu correo electrónico';
+      notValid(validAlert);
+      return false; // Detener el envío del formulario
     }
+  
+    // Comprobar si el campo de mensaje está vacío
+    if (message === '') {
+      validAlert = 'Por favor, ingresa tu mensaje';
+      notValid(validAlert);
+      return false; // Detener el envío del formulario
+    }
+  
+    // Si todos los campos son válidos, puedes enviar el formulario
+  
+    // Construir el enlace de correo electrónico
+    var mailtoLink = 'mailto:' + email + '?subject=' + encodeURIComponent(subject + ":"+ name) + '&body=' + encodeURIComponent(message);
+  
+    // Abrir el enlace de correo electrónico en una nueva pestaña o ventana
+    window.open(mailtoLink);
+  
+    // Opcional: Restablecer el formulario después de enviarlo
+    document.getElementById('contact-form').reset();
+  
+    return true; // Permitir el envío del formulario
+  }
+  
+  // Función para validar un correo electrónico utilizando una expresión regular simple
+  function validateEmail(email) {
+    var regex = /\S+@\S+\.\S+/;
+    return regex.test(email);
 
-    return true;
-}
+  }
+  
 
-function validateEmail(email) {
-    var re = /\S+@\S+\.\S+/;
-    return re.test(email);
+
+function notValid(message){
+    nameIn.innerHTML = "";
+    const h5Container = document.createElement("h5");
+    h5Container.textContent = message;
+    nameIn.append(h5Container)
 }
